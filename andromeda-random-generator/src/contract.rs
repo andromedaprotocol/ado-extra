fd0d05628398e904440c46a29396ea2435a44a61
mdjakovic0920 <mdjakovic0920@gmail.com>
2024-07-16 08:16:46 +0000

    feat: andr-cw-template is generated

---
+++ andromeda-random-generator/src/contract.rs
@@ -0,0 +1,99 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response};
+use andromeda_std::{
+    ado_base::InstantiateMsg as BaseInstantiateMsg,
+    ado_contract::{
+        ADOContract,
+    },
+    common::{actions::call_action, context::ExecuteContext},
+    error::ContractError,
+};
+
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:andromeda-random-generator";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    
+
+    let contract = ADOContract::default();
+
+    let resp = contract.instantiate(
+        deps.storage,
+        env,
+        deps.api,
+        &deps.querier,
+        info.clone(),
+        BaseInstantiateMsg {
+            ado_type: CONTRACT_NAME.to_string(),
+            ado_version: CONTRACT_VERSION.to_string(),
+            kernel_address: msg.kernel_address,
+            owner: msg.owner,
+        },
+    )?;
+
+    Ok(resp
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender)
+        )
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> Result<Response, ContractError> {
+    let ctx = ExecuteContext::new(deps, info, env);
+    if let ExecuteMsg::AMPReceive(pkt) = msg {
+        ADOContract::default().execute_amp_receive(
+            ctx,
+            pkt,
+            handle_execute,
+        )
+    } else {
+        handle_execute(ctx, msg)
+    }
+}
+
+pub fn handle_execute(
+    mut ctx: ExecuteContext,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    let action_response = call_action(
+        &mut ctx.deps,
+        &ctx.info,
+        &ctx.env,
+        &ctx.amp_ctx,
+        msg.as_ref(),
+    )?;
+
+    let res = match msg {
+        
+        _ => ADOContract::default().execute(ctx, msg)
+    }?;
+
+    Ok(res
+        .add_submessages(action_response.messages)
+        .add_attributes(action_response.attributes)
+        .add_events(action_response.events))
+}
+
+
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> Result<Binary, ContractError> {
+    match msg {
+        
+        _ => ADOContract::default().query(deps, env, msg),
+    }
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ andromeda-random-generator/src/msg.rs
@@ -0,0 +1,15 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use andromeda_std::{andr_exec, andr_instantiate, andr_query};
+
+#[andr_instantiate]
+#[cw_serde]
+pub struct InstantiateMsg {}
+
+#[andr_exec]
+#[cw_serde]
+pub enum ExecuteMsg {}
+
+#[andr_query]
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {}
